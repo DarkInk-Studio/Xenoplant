70beb5951a60468470ce89a0a18a3ac56fcafcd1
TBS89 <thaisenschueler1@gmail.com>
2024-03-08 16:30:55 +0100

    Adding comments
    + Adding comments to explain some code

--- Assets/Scripts/ApplyForce.cs
+++ Assets/Scripts/ApplyForce.cs
@@ -13,31 +13,30 @@ public class ApplyForce : MonoBehaviour
         Rigidbody rb = GetComponent<Rigidbody>();
         if(rb != null)
         {
-            rb.AddForce(force);
+            rb.AddForce(force); // Add force to the lamp; 3D
         }
         else
         {
             Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
             if(rb2D != null )
             {
-                rb2D.AddForce(force);
+                rb2D.AddForce(force); // Add force to the lamp; 2D
             }
         }
     }
 
     private void Start()
     {
-        StartCoroutine(WaitAndApply(20));
+        StartCoroutine(WaitAndApply(20)); // Wait 20 seconds then apply the first force to the lamps
     }
 
     private IEnumerator WaitAndApply(float waitTime)
     {
         while (true)
         {
-            yield return new WaitForSeconds(waitTime);
-            ApplyForceToJoint();
-            print("[ApplyForce]: Applyed force!");
-            waitTime = Random.Range(20, 60);
+            yield return new WaitForSeconds(waitTime); // Wait
+            ApplyForceToJoint(); // Apply force
+            waitTime = Random.Range(20, 60); // Generate random wait time
         }
     }
 
