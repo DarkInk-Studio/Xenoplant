8e438aec9f3bca23bf298d93154601d6018cf864
TBS89YT <thaisenschueler1@gmail.com>
2024-03-27 12:51:29 +0100

    Working Dragging System

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -4,34 +4,50 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
+    public Rigidbody2D selectedObject;
+    Vector3 offset;
+    Vector3 mousePosition;
 
-    private Vector3 screenPoint;
-    private Vector3 offset;
-    public bool IsDragable = true;
+    public float maxSpeed = 10;
+    Vector2 mouseForce;
+    Vector3 lastPosition;
 
-    void OnMouseDown()
+    void Update()
     {
-        if (IsDragable) // Only do if IsDraggable == true
-        {
-            screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-            offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        if (selectedObject)
+        {
+            mouseForce = (mousePosition - lastPosition) / Time.deltaTime;
+            mouseForce = Vector2.ClampMagnitude(mouseForce, maxSpeed);
+            lastPosition = mousePosition;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject.GetComponent<Rigidbody2D>();
+                offset = selectedObject.transform.position - mousePosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+
+            selectedObject.velocity = Vector2.zero;
+            selectedObject.AddForce(mouseForce, ForceMode2D.Impulse);
+            selectedObject = null;
         }
     }
 
-    void OnMouseDrag()
+    void FixedUpdate()
     {
-        if (IsDragable) // Only do if IsDraggable == true
+        if (selectedObject)
         {
-            GetComponent<Rigidbody2D>().isKinematic = true;
-            Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-
-            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-            transform.position = curPosition;
+            selectedObject.MovePosition(mousePosition + offset);
         }
-    }
-    private void OnMouseUp()
-    {
-        GetComponent<Rigidbody2D>().isKinematic = false;
     }
 }
